10376910d0e50572ea337fa9c1278a90d4f83bf6
hackertainment <259196870+hackertainment@users.noreply.github.com>
2026-09-15 02:22:40 +0100

    ls supporting symlink for -l

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -51,41 +51,119 @@ if (paths.length==0) {
     paths.push("./");
 }
 
-const RWXS = ["---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"]
 const [DIR, EXT] = process.env.LS_COLORS.replace(":*", "\n").split("\n");
 const DIRS = Object.fromEntries(new URLSearchParams(DIR.replaceAll("=", "=\x1b[").replaceAll(":", "m&")+"m"));
 const EXTS = Object.fromEntries(new URLSearchParams(EXT.replaceAll("=", "=\x1b[").replaceAll(":", "m&")));
-const UIDS = {};
-const GIDS = {};
+const RWXS = ["---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"];
+const UIDS = toEntries("/etc/passwd");
+const GIDS = toEntries("/etc/group");
 
-function getent() {
-    let passwds = fs.readFileSync("/etc/passwd", "utf8").split("\n");
-    let groups = fs.readFileSync("/etc/group", "utf8").split("\n");
+function toEntries(filepath) {
+    let lines = [];
     let fields = [];
-    let length = 0;
+    let ids = {};
 
-    for (let line of passwds) {
+    lines = fs.readFileSync(filepath, "utf8").split("\n");
+    for (let line of lines) {
         if (line.trim()!="" && !line.startsWith("#")) {
             fields = line.split(":");
-            UIDS[fields[2].toString()] = fields[0];
+            ids[fields[2].toString()] = fields[0];
         }
     }
-    for (let line of groups) {
-        if (line.trim()!="" && !line.startsWith("#")) {
-            fields = line.split(":");
-            GIDS[fields[2].toString()] = fields[0];
+
+    return ids;
+}
+
+function toLink(path, symlink) {
+    let filepath = path+"/"+symlink;
+
+    if (fs.lstatSync(filepath).isSymbolicLink()) {
+        if (symlink.includes("/")) {
+            path = path+symlink.slice(0, symlink.lastIndexOf("/")+1);
         }
+        symlink = fs.readlinkSync(filepath);
+        filepath = toLink(path, symlink);
     }
+
+    return filepath;
+}
+
+function toColor(filepath, isCheckExt) {
+    const lstats = fs.lstatSync(filepath);
+    const isExist = fs.existsSync(filepath);
+    let ansiColor = DIRS.rs;
+
+    if (lstats.isDirectory() && (lstats.mode&0o1000)!=0 && (lstats.mode&0o0002)!=0) {  // sticky other-writable directory (+t,o+w)
+        ansiColor = (DIRS.hasOwnProperty("tw") ? DIRS.tw : DIRS.rs);
+    }
+    else if (lstats.isDirectory() && (lstats.mode&0o0002)!=0) {  // other-writable directory (o+w)
+        ansiColor = (DIRS.hasOwnProperty("ow") ? DIRS.ow : DIRS.rs);
+    }
+    else if (lstats.isDirectory() && (lstats.mode&0o1000)!=0) {  // sticky directory (+t)
+        ansiColor = (DIRS.hasOwnProperty("st") ? DIRS.st : DIRS.rs);
+    }
+    else if (lstats.isDirectory()) {  // directory
+        ansiColor = (DIRS.hasOwnProperty("di") ? DIRS.di : DIRS.rs);
+    }
+    else if (lstats.isSymbolicLink() && isExist) {  // symbolic link
+        ansiColor = (DIRS.hasOwnProperty("ln") ? DIRS.ln : DIRS.rs);
+    }
+    else if (lstats.isSymbolicLink() && !isExist) {  // orphan symlink -> missing file
+        ansiColor = (DIRS.hasOwnProperty("or") ? DIRS.or : DIRS.rs);  // -> ansiColor = (DIRS.hasOwnProperty("mi") ? DIRS.or : DIRS.rs);
+    }
+    else if (lstats.isFile() && lstats.nlink>1) {  // multi-hardlink
+        ansiColor = (DIRS.hasOwnProperty("mh") ? DIRS.mh : DIRS.rs);
+    }
+    else if (lstats.isFIFO()) {  // FIFO (named pipe)
+        ansiColor = (DIRS.hasOwnProperty("pi") ? DIRS.pi : DIRS.rs);
+    }
+    else if (lstats.isSocket()) {  // socket
+        ansiColor = (DIRS.hasOwnProperty("so") ? DIRS.so : DIRS.rs);
+    }
+    else if (lstats.isSocket()) {  // door (Solaris 2.5 and later)
+        ansiColor = (DIRS.hasOwnProperty("do") ? DIRS.do : DIRS.rs);
+    }
+    else if (lstats.isBlockDevice()) {  // block device
+        ansiColor = (DIRS.hasOwnProperty("bd") ? DIRS.bd : DIRS.rs);
+    }
+    else if (lstats.isCharacterDevice()) {  // character device
+        ansiColor = (DIRS.hasOwnProperty("cd") ? DIRS.cd : DIRS.rs);
+    }
+    else if (lstats.isFile() && (lstats.mode&0o4000)!=0) {  // set user id (u+s)
+        ansiColor = (DIRS.hasOwnProperty("su") ? DIRS.su : DIRS.rs);
+    }
+    else if (lstats.isFile() && (lstats.mode&0o2000)!=0) {  // set group id (g+s)
+        ansiColor = (DIRS.hasOwnProperty("sg") ? DIRS.sg : DIRS.rs);
+    }
+    //else if (lstats.isFile() && ???) {  // TODO: file with capability
+    //    ansiColor = (DIRS.hasOwnProperty("ca") ? DIRS.ca : DIRS.rs);
+    //}
+    else if (lstats.isFile() && (lstats.mode&0o0111)!=0) {  // executable file
+        ansiColor = (DIRS.hasOwnProperty("ex") ? DIRS.ex : DIRS.rs);
+    }
+    else if (lstats.isFile() && Object.keys(EXTS).includes("*."+filepath.split(".").pop()) && isCheckExt) {
+        ansiColor = EXTS["*."+filepath.split(".").pop()];
+    }
+    else if (lstats.isFile()) {  // regular file
+        ansiColor = (DIRS.hasOwnProperty("fi") ? DIRS.fi : DIRS.rs);
+    }
+    else {  // normal (non-filename) text
+        ansiColor = (DIRS.hasOwnProperty("no") ? DIRS.fi : DIRS.rs);
+    }
+
+    return ansiColor;
 }
 
 function listPretty(isPrependSpace, path) {
     return function (filename) {
         const lstats = fs.lstatSync(path+"/"+filename);
         const isExist = fs.existsSync(path+"/"+filename);
+        let linkTo = "";
+        let linkColor = DIRS.rs;
         let prefix = "";
         let suffix = "";
 
-        // if filename has space, add single quote to it and leading space to others
+        // if a filename has space, add single quote to it and leading space to others
         if (filename.includes(" ")) {
             prefix = "'";
             suffix = "'";
@@ -94,71 +172,77 @@ function listPretty(isPrependSpace, path) {
             prefix = " ";
         }
 
-        Object.assign(DIRS, {rs: "\x1b[0m"});
-        if (lstats.isDirectory() && (lstats.mode&0o1000)!=0 && (lstats.mode&0o0002)!=0) {  // sticky other-writable directory (+t,o+w)
-            filename = (DIRS.hasOwnProperty("tw") ? DIRS.tw : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isDirectory() && (lstats.mode&0o0002)!=0) {  // other-writable directory (o+w)
-            filename = (DIRS.hasOwnProperty("ow") ? DIRS.ow : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isDirectory() && (lstats.mode&0o1000)!=0) {  // sticky directory (+t)
-            filename = (DIRS.hasOwnProperty("st") ? DIRS.st : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isDirectory()) {  // directory
-            filename = (DIRS.hasOwnProperty("di") ? DIRS.di : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isSymbolicLink() && isExist) {  // symbolic link
-            filename = (DIRS.hasOwnProperty("ln") ? DIRS.ln : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isSymbolicLink() && !isExist) {  // orphan symlink -> missing file
-            filename = (DIRS.hasOwnProperty("or") ? DIRS.or : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFile() && lstats.nlink>1) {  // multi-hardlink
-            filename = (DIRS.hasOwnProperty("mh") ? DIRS.mh : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFIFO()) {  // FIFO (named pipe)
-            filename = (DIRS.hasOwnProperty("pi") ? DIRS.pi : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isSocket()) {  // socket
-            filename = (DIRS.hasOwnProperty("so") ? DIRS.so : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isSocket()) {  // door (Solaris 2.5 and later)
-            filename = (DIRS.hasOwnProperty("do") ? DIRS.do : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isBlockDevice()) {  // block device
-            filename = (DIRS.hasOwnProperty("bd") ? DIRS.bd : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isCharacterDevice()) {  // character device
-            filename = (DIRS.hasOwnProperty("cd") ? DIRS.cd : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFile() && (lstats.mode&0o4000)!=0) {  // set user id (u+s)
-            filename = (DIRS.hasOwnProperty("su") ? DIRS.su : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFile() && (lstats.mode&0o2000)!=0) {  // set group id (g+s)
-            filename = (DIRS.hasOwnProperty("sg") ? DIRS.sg : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        //else if (lstats.isFile() && ???) {  // TODO: file with capability
-        //    filename = (DIRS.hasOwnProperty("ca") ? DIRS.ca : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        //}
-        else if (lstats.isFile() && (lstats.mode&0o0111)!=0) {  // executable file
-            filename = (DIRS.hasOwnProperty("ex") ? DIRS.ex : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFile() && Object.keys(EXTS).includes("*."+filename.split(".").pop())) {
-            filename = EXTS["*."+filename.split(".").pop()]+prefix+filename+suffix+DIRS.rs;
-        }
-        else if (lstats.isFile()) {  // regular file
-            filename = (DIRS.hasOwnProperty("fi") ? DIRS.fi : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
-        }
-        else {  // normal (non-filename) text
-            filename = (DIRS.hasOwnProperty("no") ? DIRS.no : DIRS.rs)+prefix+filename+suffix+DIRS.rs;
+        // if the file is a symlink, color the files they point to
+        if (isLong && lstats.isSymbolicLink()) {
+            linkTo = fs.readlinkSync(path+"/"+filename);
+            linkColor = (isExist ? toColor(toLink(path, filename), !fs.lstatSync(path+"/"+linkTo).isSymbolicLink()) : (DIRS.hasOwnProperty("mi") ? DIRS.mi : DIRS.rs));
         }
 
+        // PRETTY THE FILENAME :-)
+        filename = toColor(path+"/"+filename, true)+prefix+filename+suffix+DIRS.rs;
+        
+        // if long listing format, prepend the file status details (and also append -> for symlink)
         if (isLong) {
             filename = `${lstats.blocks}\t${lstats.mode}\t${lstats.nlink}\t${lstats.uid}\t${lstats.gid}\t${lstats.size}\t${lstats.mtime}\t${filename}`;
+            if (lstats.isSymbolicLink()) {
+                linkTo = (linkTo.includes(" ") ? "'" : "")+linkTo+(linkTo.includes(" ") ? "'" : "");
+                filename = filename+" -> "+linkColor+linkTo+DIRS.rs;
+            }
         }
 
         return filename;
     }
+}
+
+function toCharflag(mode) {
+    let ftype = " ";
+    let owner = "---";
+    let group = "---";
+    let other = "---";
+    let xattr = " ";
+
+    if ((mode&fs.constants.S_IFSOCK)==fs.constants.S_IFSOCK) {
+        ftype = "s";  // socket
+    }
+    else if ((mode&fs.constants.S_IFLNK)==fs.constants.S_IFLNK) {
+        ftype = "l";  // symbolic link
+    }
+    else if ((mode&fs.constants.S_IFREG)==fs.constants.S_IFREG) {
+        ftype = "-";  // regular file
+    }
+    else if ((mode&fs.constants.S_IFBLK)==fs.constants.S_IFBLK) {
+        ftype = "b";  // block-oriented device file
+    }
+    else if ((mode&fs.constants.S_IFDIR)==fs.constants.S_IFDIR) {
+        ftype = "d";  // directory
+    }
+    else if ((mode&fs.constants.S_IFCHR)==fs.constants.S_IFCHR) {
+        ftype = "c";  // character-oriented device file
+    }
+    else if ((mode&fs.constants.S_IFIFO)==fs.constants.S_IFIFO) {
+        ftype = "p";  // FIFO/pipe
+    }
+    else if ((mode&fs.constants.S_IFMT)!=0) {  // bit mask used to extract the file type code
+        ftype = "D";  // door
+    }
+    else {
+        ftype = "P";  // event port
+    }
+    owner = RWXS[(mode&fs.constants.S_IRWXU)>>6];
+    group = RWXS[(mode&fs.constants.S_IRWXG)>>3];
+    other = RWXS[mode&fs.constants.S_IRWXO];
+    if ((mode&fs.constants.S_ISUID)!=0) {
+        owner = owner[0]+owner[1]+(owner[2]=="x" ? "s" : "S");
+    }
+    if ((mode&fs.constants.S_ISGID)!=0) {
+        group = group[0]+group[1]+(group[2]=="x" ? "s" : "S");
+    }
+    if ((mode&fs.constants.S_ISVTX)!=0) {
+        group = other[0]+other[1]+(other[2]=="x" ? "t" : "T");
+    }
+    xattr = ".";  // TODO: "@" else "+" else "."
+
+    return (ftype+owner+group+other+xattr);
 }
 
 function listLong(prettyFilenames) {
@@ -168,12 +252,9 @@ function listLong(prettyFilenames) {
     let maxUlength = 0;
     let maxGlength = 0;
     let maxSize = 0;
-    let ftype = " ";
-    let owner = "---";
-    let group = "---";
-    let other = "---";
-    let xattr = " ";
+    let length = 0;
 
+    // calculate width of columns
     for (let row of prettyFilenames) {
         cols = row.split("\t");
         totalBlock = totalBlock+Number(cols[0]);
@@ -192,47 +273,7 @@ function listLong(prettyFilenames) {
     process.stdout.write("total "+Math.ceil(totalBlock/2)+"\n");
     for (let row of prettyFilenames) {
         cols = row.split("\t");
-        if ((cols[1]&fs.constants.S_IFSOCK)==fs.constants.S_IFSOCK) {
-            ftype = "s";  // socket
-        }
-        else if ((cols[1]&fs.constants.S_IFLNK)==fs.constants.S_IFLNK) {
-            ftype = "l";  // symbolic link
-        }
-        else if ((cols[1]&fs.constants.S_IFREG)==fs.constants.S_IFREG) {
-            ftype = "-";  // regular file
-        }
-        else if ((cols[1]&fs.constants.S_IFBLK)==fs.constants.S_IFBLK) {
-            ftype = "b";  // block-oriented device file
-        }
-        else if ((cols[1]&fs.constants.S_IFDIR)==fs.constants.S_IFDIR) {
-            ftype = "d";  // directory
-        }
-        else if ((cols[1]&fs.constants.S_IFCHR)==fs.constants.S_IFCHR) {
-            ftype = "c";  // character-oriented device file
-        }
-        else if ((cols[1]&fs.constants.S_IFIFO)==fs.constants.S_IFIFO) {
-            ftype = "p";  // FIFO/pipe
-        }
-        else if ((cols[1]&fs.constants.S_IFMT)!=0) {  // bit mask used to extract the file type code
-            ftype = "D";  // door
-        }
-        else {
-            ftype = "P";  // event port
-        }
-        owner = RWXS[(cols[1]>>6)&7];
-        group = RWXS[(cols[1]>>3)&7];
-        other = RWXS[(cols[1])&7];
-        if ((cols[1]&fs.constants.S_ISUID)!=0) {
-            owner = owner[0]+owner[1]+(owner[2]=="x" ? "s" : "S");
-        }
-        if ((cols[1]&fs.constants.S_ISGID)!=0) {
-            group = group[0]+group[1]+(group[2]=="x" ? "s" : "S");
-        }
-        if ((cols[1]&fs.constants.S_ISVTX)!=0) {
-            group = other[0]+other[1]+(other[2]=="x" ? "t" : "T");
-        }
-        xattr = ".";  // TODO: "@" or "+" or "."
-        process.stdout.write(ftype+owner+group+other+xattr+" ");
+        process.stdout.write(toCharflag(cols[1])+" ");
         process.stdout.write(cols[2].toString().padStart(maxNlink.toString().length, " ")+" ");
         process.stdout.write(UIDS[cols[3].toString()].padEnd(maxUlength, " ")+" ");
         process.stdout.write(GIDS[cols[4].toString()].padEnd(maxGlength, " ")+" ");
@@ -249,7 +290,7 @@ function listTabular(prettyFilenames) {
     process.stdout.write("\n");
 }
 
-getent();
+DIRS.rs = "\x1b[0m";
 for (let [i, path] of paths.entries()) {
     let prettyFilenames = [];
     let filenames = fs.readdirSync(path);
